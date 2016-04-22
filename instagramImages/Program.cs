576f9ed4b065e7e18422ba3f39c1d06ae4ca8853
Uğur Öztürk <tarantulaugur@hotmail.com>
2016-04-22 17:25:45 +0300

    Gerekli Fonksiyon yazıldı

--- instagramImages/Program.cs
+++ instagramImages/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace instagramImages
 {
@@ -10,6 +11,42 @@ namespace instagramImages
     {
         static void Main(string[] args)
         {
+            string urlAddress = "https://www.instagram.com/explore/tags/hdss/"; //tagı dinamik olarak değiştir.
+            string regexi = string.Format("display_src\":\"(.*?)\"");
+
+            string data = "";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, System.Text.Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+            MatchCollection mth = Regex.Matches (data,regexi);
+            
+
+            foreach (Match item in mth)
+            {
+                Console.WriteLine(item.Groups[1].Value); // Gelen Değeri kontrol et. Dilersen "\" karakterini gelen değerden silebilirsin.
+            }
+
+            Console.ReadKey();
         }
     }
 }
